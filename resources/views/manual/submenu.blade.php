a06d96d494acd465cdbf05b67d041a3715dd51dd
WiebedeBoer <Wiebe81@gmail.com>
2020-03-06 18:02:36 +0100

    manual submenu

--- resources/views/manual/submenu.blade.php
+++ resources/views/manual/submenu.blade.php
@@ -1,3 +1,8 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+
 <div class="context-menu">
     <div class="container">
 
@@ -23,4 +28,9 @@
 </div>
 
 </div>
+</div>
+
+			</div>
+        </div>
+    </div>
 </div>
