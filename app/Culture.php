ad10a4a135c2f9d963f192986065e387e6798c34
WiebedeBoer <Wiebe81@gmail.com>
2020-03-08 02:07:12 +0100

    model fkey

--- app/Culture.php
+++ app/Culture.php
@@ -9,4 +9,10 @@ class Culture extends Model
     //cultures tabel
     protected $table = 'cultures';
     protected $primaryKey = 'culture_id';
+	
+    public function regions()
+    {
+        return $this->hasMany('App\Region','region_id');
+    }	
+	
 }
--- app/Region.php
+++ app/Region.php
@@ -9,4 +9,15 @@ class Region extends Model
     //regions tabel
     protected $table = 'regions';
     protected $primaryKey = 'region_id';
+	
+    public function cultures()
+    {
+        return $this->belongsTo('App\Culture','culture');
+    } 
+
+    public function places()
+    {
+        return $this->hasMany('App\Place','place_id');
+    }	
+	
 }
