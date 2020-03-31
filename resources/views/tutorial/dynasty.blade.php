8f7cb58448d76f82b0ad8e465d55bc790d9d5e87
WiebedeBoer <wiebe81@gmail.com>
2020-03-31 22:27:07 +0200

    dynasty tutor

--- resources/views/tutorial/dynasty.blade.php
+++ resources/views/tutorial/dynasty.blade.php
@@ -6,21 +6,38 @@ Game
 <h1>Tutorial</h1>
 <h2 class="c">(Step 1) Set up a Dynasty</h2>
 	
-	<div class="row">
-		<div class="col-12 d-flex justify-content-center py-3">	
-			<div>				
-				<a href="/dynasty/create?color=back_ochre&emblem=brown_boar&shape=shield" class="btn btn-primary">Create Dynasty</a>
-			</div>
-		</div>		
-	</div>	
-	
-	<div class="row">
-		<div class="col-12 d-flex justify-content-center py-3">	
-	
+<div class="row">
+	<div class="col-12 d-flex justify-content-center py-3">	
+		<div>				
+			<a href="/dynasty/create?color=back_ochre&emblem=brown_boar&shape=shield" class="btn btn-primary">Create Dynasty</a>
+		</div>
+	</div>		
+</div>	
 
-
-
+<div class="row">
+	<div class="col-12 d-flex justify-content-center py-3">	
+<h2 class="c">Getting started</h2>
+<p>Congratulations on registering for Medieval. Three basic steps need to be completed before you can fully play Medieval. These steps are:</p>
+ <ul>
+ <li>creating a dynasty,</li> 
+ <li>creating player character,</li>
+ <li>and creating an estate.</li>
+</ul> 
+ <p>This tutorial will guide you through this process.</p>	
+	</div>
 </div>
+	
+<div class="row">
+	<div class="col-12 d-flex justify-content-center py-3">	
+<h2 class="c">About Creating a Dynasty</h2>
+<p></p>	
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-12 d-flex justify-content-center py-3">	
+	
+	</div>
 </div>
 
 @endsection
