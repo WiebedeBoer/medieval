6fddb4b9a89782d723967ae91fc2ab378e03cc32
WiebedeBoer <Wiebe81@gmail.com>
2020-12-06 02:56:35 +0100

    titles controller setup

---
+++ app/Http/Controllers/TitleController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TitleController extends Controller
+{
+    //election
+    public function election()
+    {            
+      
+    }
+
+    //feudal ranks
+    public function feudal()
+    {            
+     
+    }
+
+     //vassalize
+    public function vassal()
+    {            
+     
+    }  
+
+    //culture
+    public function culture()
+    {            
+       
+    }
+
+    //religion
+    public function religion()
+    {            
+       
+    }
+
+    //social estates
+    public function estate()
+    {            
+       
+    }
+
+    //region owner
+    public function region()
+    {            
+       
+    }
+
+    //place owner
+    public function place()
+    {            
+       
+    }
+
+    //quarter owner
+    public function quarter()
+    {            
+       
+    }
+
+
+}
