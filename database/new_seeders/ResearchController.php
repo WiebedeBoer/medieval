5dfd01551977f2c5782945f6e2215968c3949505
WiebedeBoer <wiebe81@gmail.com>
2024-01-15 21:24:43 +0100

    1344 court actions

--- database/new_seeders/ResearchController.php
+++ database/new_seeders/ResearchController.php
@@ -22,45 +22,125 @@ class ResearchController extends Controller
         string $masterCategory,
     ): array {
         switch ($masterCategory) {
-            case "Admiral":
-                $masters['technology'] = ['Port'];
-                $masters['actions'] = ['Invite_Captain','Launch_Ship','Sail'];
-              break;
             case "Steward":
+                $stewardActions = [];
+                $stewardActions['charisma'] = ''; //recruit
+                $stewardActions['organization'] = 'Collect Taxes'; //collect taxes
+                $stewardActions['agriculture'] = 'Corvee'; //improve agriculture and gather resources
+                $stewardActions['industry'] = 'Craft'; //improve industry and craft items 
+                $stewardActions['commerce'] = 'Trade'; //improve commerce and enact landbound trade                                                         
+                $stewardActions['justice'] = 'Hold Court'; //improve justice and hold court               
+                $stewardActions['defenses'] = '';          
                 $masters['technology'] = ['Crafts','Manorialism'];
-                $masters['actions'] = ['Collect_Taxes','Craft','Trade'];
-              break;
-            case "Chamberlain":
-                $masters['technology'] = ['Wardrobe'];
-                $masters['actions'] = ['Ennoblement','Debut','Plot','Network','Hold_Court'];
-              break;
-            case "Master of the Revels":
-                $masters['technology'] = ['Entertainment'];
-                $masters['actions'] = ['Feast','Fairs','Hunt','Joust'];
-              break;
-            case "Chronicler":
-                $masters['technology'] = ['Scriptures'];
-                $masters['actions'] = ['Survey','Illustrate','Tonsure'];
+                $masters['actions'] = $stewardActions;
               break;
             case "Chancellor":
+                $chancellorActions = [];
+                $chancellorActions['charisma'] = 'Ordain'; //recruit priest
+                $chancellorActions['organization'] = 'Claim'; //claim title
+                $chancellorActions['agriculture'] = 'Heal'; //heal staff or troops or population
+                $chancellorActions['industry'] = '';
+                $chancellorActions['commerce'] = 'Herald'; //enact diplomacy               
+                $chancellorActions['justice'] = 'Proselytize'; //convert population
+                $chancellorActions['defenses'] = ''; 
                 $masters['technology'] = ['Feudalism','Monasticism','Scholasticism'];
-                $masters['actions'] = ['Herald','Claim','Proselytize','Ordain','Heal'];
+                $masters['actions'] = $chancellorActions;
               break;
-            case "Engineer":
-                $masters['technology'] = ['Siegecraft'];
-                $masters['actions'] = ['Besiege','Restore'];
-              break;
-            case "Master Mason":
-                $masters['technologies'] = ['Construction','Fortification'];
-                $masters['actions'] = ['Construct','Fortify'];
+            case "Chamberlain":
+                $chamberlainActions = [];
+                $chamberlainActions['charisma'] = 'Ennoblement'; //recruit noble
+                $chamberlainActions['organization'] = 'Invite Debutante'; //recruit maiden
+                $chamberlainActions['agriculture'] = ''; //
+                $chamberlainActions['industry'] = ''; //
+                $chamberlainActions['commerce'] = 'Network'; //improve spy network           
+                $chamberlainActions['justice'] = 'Plot'; //scheme or plot or assassinate            
+                $chamberlainActions['defenses'] = ''; //
+                $masters['technology'] = ['Wardrobe'];
+                $masters['actions'] = $chamberlainActions;
               break;
             case "Constable":
+                $constableActions = [];
+                $constableActions['charisma'] = 'Adoubement'; //recruit knight
+                $constableActions['organization'] = ''; //
+                $constableActions['agriculture'] = ''; //
+                $constableActions['industry'] = 'Muster'; //muster cavalry
+                $constableActions['commerce'] = 'Charge'; //cavalry charge                
+                $constableActions['justice'] = 'Guard'; //guard settlement or holding              
+                $constableActions['defenses'] = ''; //
                 $masters['technologies'] = ['Cavalry'];
-                $masters['actions'] = ['Muster','Charge','Adoubement'];
+                $masters['actions'] = $constableActions;
               break;
             case "Marshall":
+                $marshallActions = [];
+                $marshallActions['charisma'] = 'Levy'; //recruit infantry
+                $marshallActions['organization'] = 'Train'; //train troops
+                $marshallActions['agriculture'] = 'Battle'; //enact a battle
+                $marshallActions['industry'] = 'Arm'; //arm troops
+                $marshallActions['commerce'] = 'Campaign'; //move army and launch army              
+                $marshallActions['justice'] = 'Raid'; //launch a raid              
+                $marshallActions['defenses'] = 'Skirmish'; //enact a skirmish
                 $masters['technologies'] = ['Skirmish','Melee_Polearm','Melee_Sword','Helmets','Armor'];
-                $masters['actions'] = ['Levy','Train','Campaign','Skirmish','Battle'];
+                $masters['actions'] = $marshallActions;
+              break;
+            case "Admiral":
+                $admiralActions = [];
+                $admiralActions['charisma'] = 'Invite Captain'; //recruit navy
+                $admiralActions['organization'] = ''; //
+                $admiralActions['agriculture'] = ''; //
+                $admiralActions['industry'] = 'Launch Ship'; //build vessel
+                $admiralActions['commerce'] = 'Sail'; //enact seabound trade            
+                $admiralActions['justice'] = 'Plunder'; //pirate seabound trade               
+                $admiralActions['defenses'] = 'Blockade'; //blockade port
+                $masters['technology'] = ['Port'];
+                $masters['actions'] = $admiralActions;
+              break;
+            case "Master of the Revels":
+                $revelActions = [];
+                $revelActions['charisma'] = 'Hire Minstrel'; //recruit entertainer
+                $revelActions['organization'] = ''; //
+                $revelActions['agriculture'] = 'Feast'; //hold feast
+                $revelActions['industry'] = 'Hunt'; //hold hunt 
+                $revelActions['commerce'] = 'Fairs'; //hold trade fairs              
+                $revelActions['justice'] = ''; //             
+                $revelActions['defenses'] = 'Joust'; //hold tournament 
+                $masters['technology'] = ['Entertainment'];
+                $masters['actions'] = $revelActions;
+              break;
+            case "Chronicler":
+                $chroniclerActions = [];
+                $chroniclerActions['charisma'] = 'Tonsure'; //recruit monk
+                $chroniclerActions['organization'] = ''; //
+                $chroniclerActions['agriculture'] = ''; //
+                $chroniclerActions['industry'] = 'Illustrate'; //create great book 
+                $chroniclerActions['commerce'] = ''; //           
+                $chroniclerActions['justice'] = 'Survey'; //create survey
+                $chroniclerActions['defenses'] = ''; // 
+                $masters['technology'] = ['Scriptures'];
+                $masters['actions'] = $chroniclerActions;
+              break;
+            case "Master Mason":
+                $builderActions = [];
+                $builderActions['charisma'] = ''; //recruit
+                $builderActions['organization'] = ''; //
+                $builderActions['agriculture'] = ''; //
+                $builderActions['industry'] = 'Construct'; //construct buildings and holdings
+                $builderActions['commerce'] = 'Upgrade'; //improve buildings and holdings        
+                $builderActions['justice'] = ''; //          
+                $builderActions['defenses'] = 'Fortify'; //construct castles and fortifications 
+                $masters['technologies'] = ['Construction','Fortification'];
+                $masters['actions'] = $builderActions;
+              break;
+            case "Engineer":
+                $engineerActions = [];
+                $engineerActions['charisma'] = ''; //recruit
+                $engineerActions['organization'] = ''; //
+                $engineerActions['agriculture'] = ''; //
+                $engineerActions['industry'] = 'Besiege'; //create siege equipment
+                $engineerActions['commerce'] = ''; //               
+                $engineerActions['justice'] = ''; //                
+                $engineerActions['defenses'] = 'Restore'; //restore fortifications 
+                $masters['technology'] = ['Siegecraft'];
+                $masters['actions'] = ['',''];
               break;
             default:
                 return $this->defaultMasters();
@@ -147,7 +227,7 @@ class ResearchController extends Controller
     private function defaultMasters(): array 
     {
         $defaultMasters = [];
-        $defaultMasters['master'] = '';
+        $defaultMasters['master'] = 'Employ';
         return $defaultMasters;
     }
 
