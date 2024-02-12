3921cf633a9bb3c709405b22f3f7379b9afffdd1
WiebedeBoer <wiebe81@gmail.com>
2024-02-12 21:21:52 +0100

    1352 sicily lit

--- database/new_seeders/CountySeeder.php
+++ database/new_seeders/CountySeeder.php
@@ -27,20 +27,27 @@ class CountySeeder extends Seeder
 				'marke_horse_transport' => '',		//
 				'marke_animal_husbandry' => '',		//
 				'marke_poultry' => '',				//
-													//1 migration [500], 2 carolingian [770], 3 ottonian [940], 4 romanesque [980], 5 mercantile [1070], 6 gothic [1130], 7 renaissance [1250], 8 flamboyant [1350], 9 imperial [1420] 		
-				'science_court' => '',				//1 dais, 2 lyric poetry, 3 courtly love, 4 great survey, 5 heraldry, 6 chimney, 7 rondel, 8 saltarello, 9 canvas; default=1	
-				'science_castle' => '',				//1 manor I, 2 motte, 3 enceinte, 4 donjon, 5 concentric, 6 manor II, 7 manor III, 8 manor IV, 9 manor V; default=1	
-				'science_ecclesiastical' => '',		//1	quadrivium, 2 trivium, 3 relics I, 4 groin vault, 5 studium generale, 6 lancet window, 7 ribbed vault, 8 studium humanitas, 9 relics II; default=1	
-				'science_monasticism' => '',		//1 cloister, 2 manuscripts I, 3 manuscripts II, 4 medicine, 5 passagium, 6 mendicant, 7 manuscripts III, 8 manuscripts IV, 9 manuscripts V; default=1	
-				'science_port' => '',				//1 clinker built, 2 staple right, 3 stern rudder, 4 factory I, 5 compass, 6 treadwheel crane, 7 portolan chart, 8 factory II, 9 carvel built; default=1	
-				'science_market' => '',				//1 fairs I, 2 mint right, 3 guilds, 4 fairs II, 5 fairs III, 6 merchant banking, 7 anchor currency, 8 fairs IV, fairs V; default=1	
-				'science_commerce' => '',			//1 fabrics I, 2 panemone windmill, 3 fabrics II, 4 spinning wheel, 5 horizontal loom, 6 post mill, 7 stagecoach, 8 fabrics III, block printing; default=1	
-				'science_agriculture' => '',		//1 one-field system, 2 horse collar, 3 two-field system, 4 husbandry I, 5 three-field system, 6 basket press, 7 husbandry II, 8 husbandry III, 9 husbandry IV; default=1	
-				'science_cavalry' => '',			//1 stirrup, 2 tourney, 3 horseshoe, 4 chevauchee I, 5 hastilude, 6 chivalry, 7 joust, 8 gendarme, 9 chevauchee II; default=1					
-				'science_melee' => '',				//1 round shield, 2 hafted axe, 3 knightly sword, 4 arming sword, 5 pike, 6 routier, 7 bardiche, 8 poleaxe, 9 longsword	; default=1				
-				'science_skirmish' => '',			//1 archery I, 2 archery II, 3 archery III, 4 archery IV, 5 longbow, 6 arbalest, 7 archery V, 8 pavise I, 9 matchlock; default=1	
-				'science_siege' => '',				//1 traction, 2 ram I, 3 ram II, ram III, 5 counterpoise, 6 blast furnace, 7 gunpowder, 8 cannon, 9 mantlet; default=1				
-				'science_armour' => '',				//1 hauberk I, 2 hauberk II, 3 hauberk III, 4 hauberk IV, 5 chausses, 6 coat of plates, 7 brigandine, 8 tabard, 9 plate armour; default=1						
+				'science_manorialism' => '1',		//	
+				'science_crafts' => '1',			//	
+				'science_merchantry' => '1',		//	
+				'science_scholasticism' => '1',		//	
+				'science_finance' => '1',			//	
+				'science_wardrobe' => '1',			//	
+				'science_helmets' => '1',			//
+				'science_armor' => '1',				//	
+				'science_feudalism' => '1',			//	
+				'science_cavalry' => '1',			//	
+				'science_blades' => '1',			//	
+				'science_polearms' => '1',			//	
+				'science_skirmish' => '1',			//	
+				'science_shipping' => '1',			//	
+				'science_regalement' => '1',		//	
+				'science_revelry' => '1',			//	
+				'science_architecture' => '1',		//	
+				'science_fortification' => '1',		//	
+				'science_siegecraft' => '1',		//	
+				'science_scriptures' => '1',		//	
+				'science_monasticism' => '1',		//					
 				'levy_feudal' => '',				//minimal, normal, large, maximal; default=large
 				'levy_ecclesiastical' => '',		//minimal, normal, large, maximal; default=normal
 				'levy_burgher' => '',				//minimal, normal, large, maximal; default=normal
